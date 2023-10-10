8f4f797a7fd565af488217b3ed6798b5050c8cd3
M Montserrat Gomez <montserrat.ga95@gmail.com>
2023-10-10 16:57:16 -0400

    Initial Commit

---
+++ AssignmentColor/AssignmentColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentColorApp.swift
+//  AssignmentColor
+//
+//  Created by Montserrat Gomez on 2023-10-10.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
